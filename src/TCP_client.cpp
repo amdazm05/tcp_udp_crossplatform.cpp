ebba6312a75fc145e3309efb47117bb14dbb5ed1
ahmedazee <amdali1998@yahoo.com>
2022-07-04 11:41:59 +0500

    WIP: broken state, TCP server and client not working

--- src/TCP_client.cpp
+++ src/TCP_client.cpp
@@ -1,5 +1,7 @@
 #include "TCP_client.h"
 #include <iostream>
+#include <cstring>
+
 void TCP_client::set_address()
 {
     this->server.sin_family = AF_INET;
@@ -9,10 +11,12 @@ void TCP_client::set_address()
 
 void TCP_client::socket_init()
 {
+     #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
            std::cout<<"WSAStartup has failed!";
     }
+    #endif
 
     this-> set_address();
     this-> so = socket(AF_INET,SOCK_STREAM,0);
@@ -21,13 +25,14 @@ void TCP_client::socket_init()
 void TCP_client::connect_to_server()
 { 
     if (connect(so, (struct sockaddr *)&server, sizeof server) < 0)
-            std::cout<<"Failed to connect";
+            std::cout<<"Failed to connect \n";
 }
 
 void TCP_client::send_server(char * message)
 { 
     while(1) 
     {
+        std::cout<<"Sending Message to [SERVER] \n";
         send(so, message, strlen(message) + 1, 0);
     }
 }
--- src/TCP_server.cpp
+++ src/TCP_server.cpp
@@ -1,5 +1,8 @@
 #include "TCP_server.h"
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
 void TCP_server::set_address()
 {
     this->server.sin_family = AF_INET;
@@ -9,18 +12,32 @@ void TCP_server::set_address()
 
 void TCP_server::socket_init()
 {
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-        std::cout<<"Failed WSAStartup.\n"; 
-    this->set_address();
-    if ((server_so = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+        if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+            std::cout<<"Failed WSAStartup.\n"; 
+        this->set_address();
+        if ((server_so = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
             std::cout<<"Failed socket.\n";
+    #endif
 
+    #if defined(unix) || defined(__unix__) || defined(__unix)
+        this->set_address();
+        std::cout<<"Address set \n";
+        if(server_so=socket(AF_INET,SOCK_STREAM,0) <0)
+            std::cout<<"Failed socket. \n";
+    #endif
 }
 
 void TCP_server::bind_server()
 {
-     if (bind(server_so, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+    #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+        if (bind(server_so, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
         std::cout<<"Bind failed.\n";
+    #endif
+    #if defined(unix) || defined(__unix__) || defined(__unix)
+        if (bind(server_so, (struct sockaddr *)&server, sizeof(server)) <0)
+            std::cout<<"Bind failed.\n";
+    #endif
 }
 
 void handle_client(int client_socket)
@@ -39,24 +56,27 @@ void handle_client(int client_socket)
             break;
         }
     }
-
-    closesocket(client_socket);
+    #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+        closesocket(client_socket);
+    #endif
 }
 
 void TCP_server::listen_server()
 {
-    if (listen(server_so, 100) < 0)
+    if (listen(server_so, 10) < 0)
         std::cout<<"Listen failed.\n";
-   while (1)
+    while (1)
     {
         client_length = sizeof(client);
-
-        if ((client_so = accept(server_so, (struct sockaddr *)&client, &client_length)) < 0)
+        std::cout<<client_length<<std::endl;
+        if ((client_so = accept(server_so, (struct sockaddr *)&client, &client_length))<0)
+        {   
+            std::cout<<client_so<<std::endl;
             std::cout<<("Error on accept.\n");
-
+            break;
+        }
+        
         printf("New connection accepted %s\n", inet_ntoa(client.sin_addr));
         handle_client(client_so);
     }
-
-    
 }
